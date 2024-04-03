3f9b77102de47ad370e7f7feb364e2eb16c98e5a
caio melloni <caiomelloni.contato@gmail.com>
2024-04-03 10:32:35 -0300

    adds colors

--- Sources/Assets/Colors.swift
+++ Sources/Assets/Colors.swift
@@ -8,13 +8,15 @@
 import Foundation
 import SwiftUI
 
+let bundle = Bundle.module
+
 @available(macOS 10.15, *)
 public struct Colors {
     public static let purple_50 = Color("purple-50")
     public static let purple_100 = Color("purple-100")
     public static let purple_200 = Color("purple-200")
     public static let purple_300 = Color("purple-300")
-    public static let purple_400 = Color("purple-400")
+    public static let purple_400 = Color("purple-400", bundle: bundle)
     public static let purple_500 = Color("purple-500")
     public static let purple_600 = Color("purple-600")
     public static let purple_700 = Color("purple-700")

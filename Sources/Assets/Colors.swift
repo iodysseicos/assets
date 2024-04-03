2b841b6ea3d1981fde68c21667048abbc0fc1675
Pamella <pamella.as.304@gmail.com>
2024-04-03 11:29:51 -0300

    atualizada a padronização de cores

--- Sources/Assets/Colors.swift
+++ Sources/Assets/Colors.swift
@@ -12,14 +12,44 @@ let bundle = Bundle.module
 
 @available(macOS 10.15, *)
 public struct Colors {
-    public static let purple_50 = Color("purple-50")
-    public static let purple_100 = Color("purple-100")
-    public static let purple_200 = Color("purple-200")
-    public static let purple_300 = Color("purple-300")
-    public static let purple_400 = Color("purple-400", bundle: bundle)
-    public static let purple_500 = Color("purple-500")
-    public static let purple_600 = Color("purple-600")
-    public static let purple_700 = Color("purple-700")
-    public static let purple_800 = Color("purple-800")
-    public static let purple_900 = Color("purple-900")
+    
+    //Backgrunds
+    public static let backgroundColor = Color("BackgroundColorLight", bundle: bundle)
+    public static let componentsBackgroundColor = Color("white", bundle: bundle)
+    
+    //Fonts
+    public static let textColor = Color("grey-800", bundle: bundle)
+    public static let titleColor = Color("purple-800", bundle: bundle)
+    
+    
+    public static let markedItemColor =  Color("purple-600", bundle: bundle)
+    public static let unmarkedItemColor = Color("grey-300", bundle: bundle)
+    
+    //PeriodRelated
+    public static let periodPrimaryColor = Color("red-200", bundle: bundle)
+    public static let periodSecondaryColor = Color("red-100", bundle: bundle)
+    public static let periodTerciaryColor = Color("red-50", bundle: bundle)
+    public static let periodProgressColor = Color("red-400", bundle: bundle)
+
+    //Ovulatory/Fertile Phase related
+    public static let ovulatoryPhasePrimaryColor = Color("green-200", bundle: bundle)
+    public static let ovulatoryPhaseSecondaryColor = Color("green-100", bundle: bundle)
+    public static let ovulatoryPhaseTerciaryColor = Color("green-50", bundle: bundle)
+    public static let povulatoryPhaseProgressColor = Color("green-400", bundle: bundle)
+    
+    //luteal Phase related
+    public static let lutealPhasePrimaryColor = Color("pink-200", bundle: bundle)
+    public static let lutealPhaseSecondaryColor = Color("pink-100", bundle: bundle)
+    public static let lutealPhaseTerciaryColor = Color("pink-50", bundle: bundle)
+    public static let lutealPhaseProgressColor = Color("pink-400", bundle: bundle)
+    
+    //folicular Phase related
+    public static let folicularPhasePrimaryColor = Color("blue-200", bundle: bundle)
+    public static let folicularPhaseSecondaryColor = Color("blue-100", bundle: bundle)
+    public static let folicularPhaseTerciaryColor = Color("blue-50", bundle: bundle)
+    public static let folicularPhaseProgressColor = Color("blue-400", bundle: bundle)
+    
+    
+    
+    
 }

b12c23125d63794f5ecc6813bdb08bb932b923b5
caio melloni <caiomelloni.contato@gmail.com>
2024-04-03 09:53:05 -0300

    adds purple colors

---
+++ Sources/Assets/Colors.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  
+//
+//  Created by Caio Melloni dos Santos on 03/04/24.
+//
+
+import Foundation
+import SwiftUI
+
+@available(macOS 10.15, *)
+public struct Colors {
+    public static let purple_50 = Color("purple-50")
+    public static let purple_100 = Color("purple-100")
+    public static let purple_200 = Color("purple-200")
+    public static let purple_300 = Color("purple-300")
+    public static let purple_400 = Color("purple-400")
+    public static let purple_500 = Color("purple-500")
+    public static let purple_600 = Color("purple-600")
+    public static let purple_700 = Color("purple-700")
+    public static let purple_800 = Color("purple-800")
+    public static let purple_900 = Color("purple-900")
+}

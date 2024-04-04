a0c34a02b8821aa7ac1703ee9af6d6ac2387c9c8
Pamella <pamella.as.304@gmail.com>
2024-04-04 09:37:43 -0300

    alterada a padronizacao do nome de cores

--- Sources/Assets/Colors.swift
+++ Sources/Assets/Colors.swift
@@ -13,42 +13,96 @@ let bundle = Bundle.module
 @available(macOS 10.15, *)
 public struct Colors {
     
-    //Backgrunds
-    public static let backgroundColor = Color("BackgroundColorLight", bundle: bundle)
-    public static let componentsBackgroundColor = Color("white", bundle: bundle)
+    public static let purple_50 = Color("purple-50", bundle: bundle)
+    public static let purple_100 = Color("purple-100", bundle: bundle)
+    public static let purple_200 = Color("purple-200", bundle: bundle)
+    public static let purple_300 = Color("purple-300", bundle: bundle)
+    public static let purple_400 = Color("purple-400", bundle: bundle)
+    public static let purple_500 = Color("purple-500", bundle: bundle)
+    public static let purple_600 = Color("purple-600", bundle: bundle)
+    public static let purple_700 = Color("purple-700", bundle: bundle)
+    public static let purple_800 = Color("purple-800", bundle: bundle)
+    public static let purple_900 = Color("purple-900", bundle: bundle)
     
-    //Fonts
-    public static let textColor = Color("grey-800", bundle: bundle)
-    public static let titleColor = Color("purple-800", bundle: bundle)
+    public static let green_50 = Color("green-50", bundle: bundle)
+    public static let green_100 = Color("green-100", bundle: bundle)
+    public static let green_200 = Color("green-200", bundle: bundle)
+    public static let green_300 = Color("green-300", bundle: bundle)
+    public static let green_400 = Color("green-400", bundle: bundle)
+    public static let green_500 = Color("green-500", bundle: bundle)
+    public static let green_600 = Color("green-600", bundle: bundle)
+    public static let green_700 = Color("green-700", bundle: bundle)
+    public static let green_800 = Color("green-800", bundle: bundle)
+    public static let green_900 = Color("green-900", bundle: bundle)
     
+    public static let grey_50 = Color("grey-50", bundle: bundle)
+    public static let grey_100 = Color("grey-100", bundle: bundle)
+    public static let grey_200 = Color("grey-200", bundle: bundle)
+    public static let grey_300 = Color("grey-300", bundle: bundle)
+    public static let grey_400 = Color("grey-400", bundle: bundle)
+    public static let grey_500 = Color("grey-500", bundle: bundle)
+    public static let grey_600 = Color("grey-600", bundle: bundle)
+    public static let grey_700 = Color("grey-700", bundle: bundle)
+    public static let grey_800 = Color("grey-800", bundle: bundle)
+    public static let grey_900 = Color("grey-900", bundle: bundle)
     
-    public static let markedItemColor =  Color("purple-600", bundle: bundle)
-    public static let unmarkedItemColor = Color("grey-300", bundle: bundle)
+    public static let orange_50 = Color("orange-50", bundle: bundle)
+    public static let orange_100 = Color("orange-100", bundle: bundle)
+    public static let orange_200 = Color("orange-200", bundle: bundle)
+    public static let orange_300 = Color("orange-300", bundle: bundle)
+    public static let orange_400 = Color("orange-400", bundle: bundle)
+    public static let orange_500 = Color("orange-500", bundle: bundle)
+    public static let orange_600 = Color("orange-600", bundle: bundle)
+    public static let orange_700 = Color("orange-700", bundle: bundle)
+    public static let orange_800 = Color("orange-800", bundle: bundle)
+    public static let orange_900 = Color("orange-900", bundle: bundle)
     
-    //PeriodRelated
-    public static let periodPrimaryColor = Color("red-200", bundle: bundle)
-    public static let periodSecondaryColor = Color("red-100", bundle: bundle)
-    public static let periodTerciaryColor = Color("red-50", bundle: bundle)
-    public static let periodProgressColor = Color("red-400", bundle: bundle)
+  
+    public static let pink_50 = Color("pink-50", bundle: bundle)
+    public static let pink_100 = Color("pink-100", bundle: bundle)
+    public static let pink_200 = Color("pink-200", bundle: bundle)
+    public static let pink_300 = Color("pink-300", bundle: bundle)
+    public static let pink_400 = Color("pink-400", bundle: bundle)
+    public static let pink_500 = Color("pink-500", bundle: bundle)
+    public static let pink_600 = Color("pink-600", bundle: bundle)
+    public static let pink_700 = Color("pink-700", bundle: bundle)
+    public static let pink_800 = Color("pink-800", bundle: bundle)
+    public static let pink_900 = Color("pink-900", bundle: bundle)
+    
+    public static let red_50 = Color("red-50", bundle: bundle)
+    public static let red_100 = Color("red-100", bundle: bundle)
+    public static let red_200 = Color("red-200", bundle: bundle)
+    public static let red_300 = Color("red-300", bundle: bundle)
+    public static let red_400 = Color("red-400", bundle: bundle)
+    public static let red_500 = Color("red-500", bundle: bundle)
+    public static let red_600 = Color("red-600", bundle: bundle)
+    public static let red_700 = Color("red-700", bundle: bundle)
+    public static let red_800 = Color("red-800", bundle: bundle)
+    public static let red_900 = Color("red-900", bundle: bundle)
+    
+    public static let white_50 = Color("white-50", bundle: bundle)
+    public static let white_100 = Color("white-100", bundle: bundle)
+    public static let white_200 = Color("white-200", bundle: bundle)
+    public static let white_300 = Color("white-300", bundle: bundle)
+    public static let white_400 = Color("white-400", bundle: bundle)
+    public static let white_500 = Color("white-500", bundle: bundle)
+    public static let white_600 = Color("white-600", bundle: bundle)
+    public static let white_700 = Color("white-700", bundle: bundle)
+    public static let white_800 = Color("white-800", bundle: bundle)
+    public static let white_900 = Color("white-900", bundle: bundle)
+    
+    public static let yellow_50 = Color("yellow-50", bundle: bundle)
+    public static let yellow_100 = Color("yellow-100", bundle: bundle)
+    public static let yellow_200 = Color("yellow-200", bundle: bundle)
+    public static let yellow_300 = Color("yellow-300", bundle: bundle)
+    public static let yellow_400 = Color("yellow-400", bundle: bundle)
+    public static let yellow_500 = Color("yellow-500", bundle: bundle)
+    public static let yellow_600 = Color("yellow-600", bundle: bundle)
+    public static let yellow_700 = Color("yellow-700", bundle: bundle)
+    public static let yellow_800 = Color("yellow-800", bundle: bundle)
+    public static let yellow_900 = Color("yellow-900", bundle: bundle)
+    
 
-    //Ovulatory/Fertile Phase related
-    public static let ovulatoryPhasePrimaryColor = Color("green-200", bundle: bundle)
-    public static let ovulatoryPhaseSecondaryColor = Color("green-100", bundle: bundle)
-    public static let ovulatoryPhaseTerciaryColor = Color("green-50", bundle: bundle)
-    public static let povulatoryPhaseProgressColor = Color("green-400", bundle: bundle)
-    
-    //luteal Phase related
-    public static let lutealPhasePrimaryColor = Color("pink-200", bundle: bundle)
-    public static let lutealPhaseSecondaryColor = Color("pink-100", bundle: bundle)
-    public static let lutealPhaseTerciaryColor = Color("pink-50", bundle: bundle)
-    public static let lutealPhaseProgressColor = Color("pink-400", bundle: bundle)
-    
-    //folicular Phase related
-    public static let folicularPhasePrimaryColor = Color("blue-200", bundle: bundle)
-    public static let folicularPhaseSecondaryColor = Color("blue-100", bundle: bundle)
-    public static let folicularPhaseTerciaryColor = Color("blue-50", bundle: bundle)
-    public static let folicularPhaseProgressColor = Color("blue-400", bundle: bundle)
-    
     
     
     
